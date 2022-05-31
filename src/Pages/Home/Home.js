013bbdc38a45cecc69c4b5bd62da81acd48395a5
Pawan Bhatta <pawanbhatta00@gmail.com>
2022-05-31 11:18:03 +0545

    home page completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Featured from "../../components/Featured/Featured";
 import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import MailList from "../../components/MailList/MailList";
 import Navbar from "../../components/Navbar/Navbar";
@@ -19,6 +20,7 @@ const Home = () => {
         <h1 className="homeTitle">Home guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
     </div>
   );
